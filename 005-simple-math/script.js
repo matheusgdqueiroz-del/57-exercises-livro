989e4ba1b138dc6f1de667312340f4cf1d691cbd
Matheus <matheusgdqueiroz@gmail.com>
2026-06-26 17:17:11 -0300

    Pending: polish

--- 005-simple-math/script.js
+++ 005-simple-math/script.js
@@ -1,9 +1,14 @@
 const xInput = document.querySelector("#first-number");
 const yInput = document.querySelector("#second-number");
-const resultDisplay = document.querySelector("#results");
+sumDisplay = document.querySelector("#sum");
+subDisplay = document.querySelector("#sub");
+mulDisplay = document.querySelector("#mul");
+divDisplay = document.querySelector("#div");
+sumOperationDisplay = document.querySelector("#sum-operation");
+subOperationDisplay = document.querySelector("#sub-operation");
+mulOperationDisplay = document.querySelector("#mul-operation");
+divOperationDisplay = document.querySelector("#div-operation");
 const errorDisplay = document.querySelector("#error");
-
-
 
 function validateInput(input) {
     if (input.trim() === "" ) {
@@ -24,28 +29,49 @@ function calculate() {
     const y = validateInput(yInput.value);
     if (x !== null && y !== null) {
         errorDisplay.textContent = ""
+        const results = [];
+        results.push(x + y, x - y, x * y, x / y );
+        return results;
     }
-    results = [];
-    results.push(x + y);
-    results.push(x - y);
-    results.push(x * y);
-    results.push(x / y);
-    return results;
+    return null;
 }
 
 [xInput, yInput].forEach(input => {
     input.addEventListener("input", ()=> {
     
     const results = calculate()
-    let [sum, sub, mul, div] = results;
     
-    resultDisplay.textContent = `
-    Sum = ${sum} 
-    Subtraction = ${sub} 
-    Multiplication = ${mul} 
-    Division = ${div.toFixed(2)} 
-    `
+    if (results !== null) {
+        let [sum, sub, mul, div] = results;
+        
+        sumDisplay.textContent = `${sum}`;
+        subDisplay.textContent = `${sub}`;
+        mulDisplay.textContent = `${mul}`;
+        if (div !== Infinity) {
+            divDisplay.textContent = `${div.toFixed(2)}`;
+        }else {
+            divDisplay.textContent = "Can't divide by zero";
+        } 
+        sumOperationDisplay.innerHTML = `${xInput.value} + ${yInput.value}`
+        subOperationDisplay.innerHTML = `${xInput.value} - ${yInput.value}`
+        mulOperationDisplay.innerHTML = `${xInput.value} x ${yInput.value}`
+        divOperationDisplay.innerHTML = `${xInput.value} / ${yInput.value}`
+
+    } 
+    
     })
 })
 
+        // OperationDisplay.innerHTML = `
+        // <p>${xInput.value} + ${yInput.value}</p>
+        // <p>${xInput.value} - ${yInput.value}</p>
+        // <p>${xInput.value} x ${yInput.value}</p>
+        // <p>${xInput.value} / ${yInput.value}</p>
+        // `
 
+    //     else {
+    //     sumDisplay.textContent = "";
+    //     subDisplay.textContent = "";
+    //     mulDisplay.textContent = "";
+    //     divDisplay.textContent = "";
+    // }
